29a2d8cbd9b90c5f39c09f2ed0e2491ba6badd9b
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2023-06-09 21:02:25 +0200

    Make layout and style of the calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalculatorButtonComponent } from './calculator-button/calculator-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorButtonComponent
   ],
   imports: [
     BrowserModule,
