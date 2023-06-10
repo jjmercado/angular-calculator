463844936bd46764ba2ba1f8d07441d0c7a57f4e
Jeremy Mercado <jeremy.j.mercado@hotmail.com>
2023-06-10 17:04:26 +0200

    Add click and keyboard events to buttons and show output

--- src/app/calculator-button/calculator-button.component.ts
+++ src/app/calculator-button/calculator-button.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-calculator-button',
   templateUrl: './calculator-button.component.html',
   styleUrls: ['./calculator-button.component.css']
 })
-export class CalculatorButtonComponent {
+export class CalculatorButtonComponent 
+{
+  outputs: string[] = [];
+  calcButtons: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "/", "*", "-", "+", ","];
+  
+  @HostListener('document:keypress', ['$event'])
+  onKeypress(event: KeyboardEvent)
+  {
+    console.log(event.key);
+    if(event.key === "Enter")
+    {
+      console.log("ENTER");
+    }
+    else
+    {
+      this.calcButtons.forEach(element => 
+        {
+          if (event.key === element) 
+          {
+            this.clickButton(event.key);  
+          }  
+        });
+    }
+  }
 
+  @HostListener('document:keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent)
+  {
+    if(event.key === "Backspace")
+    {
+      console.log("Backspace");
+    }
+  }
+
+  clickButton(buttonValue: string)
+  {
+    this.outputs.push(buttonValue);
+  }
+
+  calculation(output: string[])
+  {
+
+  }
 }
